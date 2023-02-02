489f10d46120d974f083ea450bd86d5a53e5951f
Sorethea OK <sorethea@gmail.com>
2023-02-02 16:19:55 +0700

    first commit

--- Models/Address.php
+++ Models/Address.php
@@ -4,15 +4,33 @@ namespace Modules\Utility\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Address extends Model
 {
     use HasFactory;
 
-    protected $fillable = [];
-    
+    protected $fillable = [
+        "address",
+        "name",
+        "city",
+        "country",
+        "is_default",
+        "location",
+        "properties",
+    ];
+
+    protected $casts = [
+        "properties"=>"array"
+    ];
+
+    public function owner(): MorphTo{
+        return $this->morphTo();
+    }
+
     protected static function newFactory()
     {
         return \Modules\Utility\Database\factories\AddressFactory::new();
     }
+
 }
