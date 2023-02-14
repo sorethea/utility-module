e706cdb3ac3b257b301e5a775e5f8a211500c0b5
Sorethea OK <sorethea@gmail.com>
2023-02-14 14:07:48 +0700

    lam-dev

---
+++ Providers/InstallServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Modules\Utility\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Modules\Core\Providers\BaseInstallServiceProvider;
+
+class InstallServiceProvider extends BaseInstallServiceProvider
+{
+
+}
